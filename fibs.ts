8a03da6f4a26e99c6fc4a1ad50f283b04f5a8f46
Andre Weissflog <floooh@gmail.com>
2023-03-25 19:51:20 +0100

    fix fibs.ts

--- fibs.ts
+++ fibs.ts
@@ -1,6 +1,7 @@
 import * as fibs from 'https://raw.githubusercontent.com/floooh/fibs/main/mod.ts'
+//import * as fibs from '../fibs/mod.ts';
 
-export const projectDesc: fibs.ProjectDesc = {
+export const project: fibs.ProjectDesc = {
     name: 'hello-world',
     targets: {
         hello: { type: 'plain-exe', sources: ['hello.c'] }
