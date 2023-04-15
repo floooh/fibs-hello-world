35a90e5ccfdfb4920346a8879736b61fd1ee06e5
Andre Weissflog <floooh@gmail.com>
2023-04-15 15:40:53 +0200

    fix fibs.ts syntax

--- fibs.ts
+++ fibs.ts
@@ -3,10 +3,11 @@ import * as fibs from 'https://raw.githubusercontent.com/floooh/fibs/main/mod.ts
 
 export const project: fibs.ProjectDesc = {
     name: 'hello-world',
-    targets: {
-        hello: {
+    targets: [
+        {
+            name: 'hello',
             type: 'plain-exe',
             sources: () => ['hello.c']
         }
-    }
+    ]
 }
