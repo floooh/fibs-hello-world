16de4c4c5998ac78bcd8fb9f06fbb4e36d3bbe85
Andre Weissflog <floooh@gmail.com>
2025-12-21 16:35:40 +0100

    update for fibs refactoring

--- fibs.ts
+++ fibs.ts
@@ -1,23 +1,15 @@
-import * as fibs from 'https://deno.land/x/fibs@v1.6.0/mod.ts'
-//import * as fibs from '../fibs/mod.ts';
+import { fibs, Builder, Configurer } from 'https://raw.githubusercontent.com/floooh/fibs/master/index.ts';
 
-if (import.meta.main) fibs.main();
+fibs(import.meta);
 
-export const project: fibs.ProjectDesc = {
-    name: 'hello-world',
-    targets: [
-        {
-            name: 'hello',
-            type: 'plain-exe',
-            sources: () => ['hello.c']
-        }
-    ],
-    // add optional WASI platform support
-    imports: [
-        {
-            name: 'platforms',
-            url: 'https://github.com/floooh/fibs-platforms',
-            import: [ 'wasi.ts' ],
-        },
-    ],
+export function configure(c: Configurer): void {
+    c.addImport({
+        name: "wasisdk",
+        url: "https://github.com/floooh/fibs-platforms",
+        files: ["wasi.ts"],
+    });
+}
+
+export function build(b: Builder): void {
+    b.addTarget({ name: "hello", type: "plain-exe", sources: ["hello.c"] });
 }
